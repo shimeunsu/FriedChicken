92aaf2fed416c4c672233f4618f225180374e5bf
Joe Giustizia <jgiustiz@uwo.ca>
2018-11-22 16:52:51 -0500

    fixed movement bounds

--- FriedChicken/Chicken.cpp
+++ FriedChicken/Chicken.cpp
@@ -9,17 +9,25 @@ Chicken::Chicken(Vector2u size){
 }
 
 void Chicken::Move(Event e){
-    if(Keyboard::Key::Right == e.key.code){
-        shape.move(CHICKEN_MOVEMENT_AMOUNT,0);
+    Vector2f oldPos = shape.getPosition();
+    Vector2f newPos;
+    switch(e.key.code) {
+        case Keyboard::Up:
+            newPos = Vector2f(oldPos.x, oldPos.y - CHICKEN_MOVEMENT_AMOUNT);
+            break;
+        case Keyboard::Down:
+            newPos = Vector2f(oldPos.x, oldPos.y + CHICKEN_MOVEMENT_AMOUNT);
+            break;
+        case Keyboard::Left:
+            newPos = Vector2f(oldPos.x - CHICKEN_MOVEMENT_AMOUNT, oldPos.y);
+            break;
+        case Keyboard::Right:
+            newPos = Vector2f(oldPos.x + CHICKEN_MOVEMENT_AMOUNT, oldPos.y);
+            break;
     }
-    else if(Keyboard::Key::Left == e.key.code){
-        shape.move(-CHICKEN_MOVEMENT_AMOUNT,0);
-    }
-    else if(Keyboard::Key::Up == e.key.code){
-        shape.move(0,-CHICKEN_MOVEMENT_AMOUNT);
-    }
-    else if(Keyboard::Key::Down == e.key.code){
-        shape.move(0,CHICKEN_MOVEMENT_AMOUNT);
+    if (!(newPos.x > WINDOW_WIDTH  - CHICKEN_SIZE_WIDTH || newPos.x < 0
+       || newPos.y > WINDOW_HEIGHT - CHICKEN_SIZE_HEIGHT || newPos.y < 0)) {
+        shape.setPosition(newPos);
     }
 }
 
--- FriedChicken/Game.cpp
+++ FriedChicken/Game.cpp
@@ -6,27 +6,34 @@ Game::Game(){
     r = new RenderWindow(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
     chicken = new Chicken(r->getSize());
     human = new Human(r->getSize().x / 2, 0);
-    
+
 }
 
 void Game::Loop(){
     while (r->isOpen()) {
         Event e;
-        
+
         while (r->pollEvent(e)) {
-            
-            if(Event::KeyReleased){
-                
-                chicken->Move(e);
-                
+
+            switch(e.type) {
+                case Event::EventType::Closed:
+                    r->close();
+                    break;
+                case Event::EventType::KeyPressed:
+                    if (e.key.code == Keyboard::Up ||
+                        e.key.code == Keyboard::Down ||
+                        e.key.code == Keyboard::Left ||
+                        e.key.code == Keyboard::Right)
+                    chicken->Move(e);
+                    break;
             }
-            
+
             //if (e.type == Event::Closed) r->close();
- 
+
         }
         //update the game
         human->Move(r->getSize());
-        
+
         if(chicken->GetShape().getGlobalBounds().intersects(human->GetShape().getGlobalBounds())){
             r->close();
         }
@@ -36,5 +43,5 @@ void Game::Loop(){
         //insert here
         r->display();
     }
-    
+
 }
