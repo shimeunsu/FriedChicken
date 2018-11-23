69346a2b93f1caad71bfd964d6ddf67663c12c04
Eunsu Shim <shimeunsu@gmail.com>
2018-11-22 19:40:59 -0500

    la

--- FriedChicken/Game.cpp
+++ FriedChicken/Game.cpp
@@ -4,6 +4,7 @@ Game::Game() {
     r = new RenderWindow(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
     chicken = new Chicken(r->getSize());
     human = new Human(r->getSize().x / 2, 0);
+    initChicken();
 }
 
 void Game::Loop(){
@@ -36,11 +37,25 @@ void Game::Loop(){
                 r->close();
             }
             r->clear();
+        
             sprite->draw(r);
             human->draw(r);
-  //          chicken->draw(r);
+            chicken->draw(r);
             //insert here
             r->display();
     }
 
+}
+
+void Game::initChicken(){
+    Texture chicken;
+    if (!texture.loadFromFile("sprite/chicken.png")){
+                std::cout<<"load failed"<<std::endl;
+                system("pause");
+            }
+    chickenSprite.setTexture(chicken);
+    r->draw(chickenSprite);
+    
+    
+    
 }
