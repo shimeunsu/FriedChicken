c2344e2535b3eaee02ba3354c2e2b201aed746b5
Eunsu Shim <shimeunsu@gmail.com>
2018-11-24 19:50:02 -0500

    more humans

--- FriedChicken/Game.cpp
+++ FriedChicken/Game.cpp
@@ -4,7 +4,11 @@ Game::Game() {
     r = new RenderWindow(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
     chicken = new Chicken(r->getSize());
     human = new Human(r->getSize().x / 2, 0);
-    
+    human1 = new Human(r->getSize().x / 4, 0);
+    human2 = new Human(r->getSize().x / 5, 0);
+    human3 = new Human(r->getSize().x / 6, 0);
+    human4 = new Human(r->getSize().x / 7, 0);
+    human5 = new Human(r->getSize().x / 8, 0);
 }
 
 void Game::Loop(){
@@ -32,12 +36,23 @@ void Game::Loop(){
             
             //update the game
             human->Move(r->getSize());
+            human1->Move(r->getSize());
+            human2->Move(r->getSize());
+            human3->Move(r->getSize());
+            human4->Move(r->getSize());
+            human5->Move(r->getSize());
         }
         if(chicken->GetShape().getGlobalBounds().intersects(human->GetShape().getGlobalBounds())){
             r->close();
         }
         r->clear();
         human->Draw(*r);
+        human1->Draw(*r);
+        human2->Draw(*r);
+        human3->Draw(*r);
+        human4->Draw(*r);
+        human5->Draw(*r);
+        
         chicken->Draw(*r);
         //insert here
         r->display();
