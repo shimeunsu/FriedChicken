c8585c396c4a5fed9a3982e8df8c4ab3772d45c8
Eunsu Shim <shimeunsu@gmail.com>
2018-11-27 16:51:40 -0500

    score

--- FriedChicken/Game.cpp
+++ FriedChicken/Game.cpp
@@ -57,7 +57,7 @@ void Game::Loop() {
         }
         
   
-        if (chicken->GetShape()->getPosition().x > WINDOW_WIDTH){
+        if (chicken->GetShape()->getPosition().x > 500){
             gameWin(r,score);
         }
         for (Human* human: humans) {
